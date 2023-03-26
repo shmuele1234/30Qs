802ec93d83ce88ca576c7836c86488b1b49f1ba6
shmuel egert <shmuel.egert1@gmail.com>
2023-03-26 12:46:14 +0300

    did some work on the queryed data

--- large modle query/InsertIDsIntoQuery.java
+++ large modle query/InsertIDsIntoQuery.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-public class InsertNamesIntoQuery {
+public class InsertIDsIntoQuery {
     //need to add an option for adding questions for the categories.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        String[] names = new String[5000];
+        String[] ID = new String[5000];
         int i = 0;
-        System.out.println("Enter the names in separate rows: ");
+        System.out.println("Enter the IDs in separate rows: ");
 
         for (; scanner.hasNextLine(); i++){
-            names[i] = scanner.nextLine();
+            ID[i] = scanner.nextLine();
         }
         for (int j = 0; j<i; j++){
-            System.out.println("\"" + names[j] + "\" ");
+            System.out.println("wd:" + ID[j] + " ");
         }
     }
 }
